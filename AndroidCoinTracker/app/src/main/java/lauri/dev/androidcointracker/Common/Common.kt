15835b9ee42abc776b7d64d3d9a6fac91a3e43d7
Lauobrio42 <laurine.obriotbouvier@outlook.fr>
2020-01-08 14:18:32 +0100

    non mais enfin!

---
+++ AndroidCoinTracker/app/src/main/java/lauri/dev/androidcointracker/Common/Common.kt
@@ -0,0 +1,11 @@
+package edmt.dev.kotlincointracker.Common
+
+/**
+ * Created by lauri on 6/01/2020.
+ */
+
+object Common{
+    var imageUrl = "https://res.cloudinary.com/dxi90ksom/image/upload/"
+
+    var MAX_COIN_LOAD = 1000
+}
